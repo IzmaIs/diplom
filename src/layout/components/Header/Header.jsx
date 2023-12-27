ba13646ae2434cbfc55de485b3f3838fa911cf9e
Ihsan <ichestor0@gmail.com>
2023-12-27 04:48:47 +0300

    Продолжение разбработки

--- src/layout/components/Header/Header.jsx
+++ src/layout/components/Header/Header.jsx
@@ -1,18 +1,23 @@
 import classes from "./header.module.scss";
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
   return (
     <div className={classes.container}>
       <div className={classes.title}>
-        <img src="icons/icon.png" alt="logo" height={56} width={56} />
+        <NavLink to="/">
+          <img src="icons/icon.png" alt="logo" height={56} width={56} />
+        </NavLink>
         <text>АПТЕКА</text>
       </div>
       <div className={classes.search}>
-        <input />
+        <input placeholder="Быстрый поиск товаров" />
       </div>
       <div className={classes.buttons}>
         <span>И. И. ПЕТРОВА</span>
-        <button>Выход</button>
+        <button>
+          <img src="icons/sigin.svg" alt="sigin" />
+        </button>
       </div>
     </div>
   );
